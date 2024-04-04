db4c00d5b4b938d1e2c023e719f5a7114e509297
Ritika <riyachhikara26@gmail.com>
2024-04-04 12:24:09 +0530

    Initial commit

---
+++ src/database/config.js
@@ -0,0 +1,13 @@
+
+
+import mongoose from 'mongoose';
+
+const Connection = async () => {
+    try {
+       await mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true });
+        console.log("Database connected sucessfully");
+    } catch (error) {
+        console.log("Error while connecting with the database", error.message);
+    }
+}
+export default Connection;
